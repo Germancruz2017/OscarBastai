4c47006de7099d12eaf63c1a2b34ab48a474c9df
Germancruz2017 <mrgermy26@gmail.com>
2017-02-27 01:18:45 -0800

    Update sick.js

--- sick.js
+++ sick.js
@@ -1,5 +1,19 @@
-$(document).ready(function(){
-      $('KS').slick({
-        setting-name: setting-value
-      });
-    });		
+$('#carousel').slick({
+    arrows: true,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed:2500,
+    speed: 500,
+    variableWidth: true,
+    onAfterChange: function(){ 
+          var cat = ($('#carousel').slickCurrentSlide()) + 1;
+          $('.client-text > li').hide();
+          $('.client-text > li:nth-child('+ cat +')').show();
+    }
+});
+
+
+
+  $('.client-text > li').hide();
+  $('.client-text > li:first-child').show();
+
