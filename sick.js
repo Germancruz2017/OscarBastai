5705a7fc827689746259d944a731d2b08fabbf2d
Germancruz2017 <mrgermy26@gmail.com>
2017-02-27 01:00:32 -0800

    Update sick.js

--- sick.js
+++ sick.js
@@ -1,6 +1,8 @@
-$('.autoplay').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
+$(document).ready(function){
+    $('SK.autoplay').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 });
